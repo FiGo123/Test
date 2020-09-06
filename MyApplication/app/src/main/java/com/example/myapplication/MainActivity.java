5c978db8becf7581f6cd5593a8e58cb10c81d726
Filip Golovic <filip@coinis.com>
2020-09-06 18:20:44 +0200

    added test app

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //AppUpdater appUpdater = new AppUpdater(this)
+             //   .showEvery(5)
+             //   .setUpdateFrom(UpdateFrom.GITHUB)
+              //  .setGitHubUserAndRepo()
+               // .start();
+
+    }
+}
